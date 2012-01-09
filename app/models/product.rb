f7fd5b141bf02150d8e3816668e84d7389afaecc
gonglexin <gonglexin@gmail.com>
2012-01-09 16:30:41 +0800

    products admin fucntion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :category
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :description, :presence => true
+
 end
