a50b9e52a115224b1073fe9bfc835090aaaf7af9
gonglexin <gonglexin@gmail.com>
2012-08-18 00:54:13 +0800

    show up a message in login page when user not sign in

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,7 +4,7 @@ class Admin::ApplicationController < ApplicationController
 
   def require_admin
     unless Admin.find_by_id(cookies[:remember_token])
-      redirect_to admin_login_path
+      redirect_to admin_login_path, notice: "Please login in"
     end
   end
 end
