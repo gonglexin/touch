459d52f554717081926dc1d19ce43ec8e50956c5
gonglexin <gonglexin@gmail.com>
2012-08-19 13:47:44 +0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+about             = Content.new
+contact           = Content.new
+about.page_name   = 'about'
+contact.page_name = 'contact'
+
+about.body = <<-ABOUT
+<h2>About Us</h2><span>We are the best seller in China<br><br>website:&nbsp;<a target="_blank" rel="nofollow" href="http://justelec.com/">http://justelec.com/ </a><br></span>
+ABOUT
+
+contact.body = <<-CONTACT
+<h2>Contact Us</h2><br>QQ: 286896969<br>Tel: 13874838580<br>Email: gonglexin@gmail.com<br>
+CONTACT
+
+# Default contents for about & contact pages
+about.save
+contact.save
+
+# A root category
+Category.create!(name: 'root')
+# A admin user
+Admin.create!(email: 'user@example.com',
+              password: 'password_here',
+              password_confirmation: 'password_here')
--- lib/tasks/init_data.rake
+++
@@ -1,9 +0,0 @@
-namespace :db do
-  desc "Initiate database data"
-  task :initiate => :environment do
-    Category.create!(name: 'root')
-    Admin.create!(email: 'user@example.com',
-                  password: 'password_here',
-                  password_confirmation: 'password_here')
-  end
-end
