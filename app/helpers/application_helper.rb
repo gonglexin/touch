27d1ea6170b3f38c10ccfb08c025e2517f6981fb
gonglexin <gonglexin@gmail.com>
2012-08-23 21:11:55 +0800

    extract duplicate code to a method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,11 @@ module ApplicationHelper
     categories = Category.where(:parent_id => 1)
     tree << content_tag('li', link_to('All Products', products_path), :class => 'nav-header active')
     categories.each do |c|
-      tree << content_tag('li',
-                          link_to(c.name,
-                                  { :controller => 'products', :action => 'index', :category => c.id },
-                                  :class => "#{'active' if current_page?(category: c.id)}"),
-                          :class => 'nav-header')
+      tree << content_tag('li', category_link(c), :class => 'nav-header')
       tree << append_child_categories(c) unless c.subcategories.nil?
     end
     tree << '</ul>'
-    sanitize (tree)
+    sanitize tree
   end
 
   def categories_selector(form)
@@ -23,12 +19,14 @@ module ApplicationHelper
     def append_child_categories(category)
       child_tree = '<ul class="nav nav-list">'
       category.subcategories.each do |c|
-        child_tree << content_tag('li',
-                                  link_to(c.name,
-                                          { :controller => 'products', :action => 'index', :category => c.id },
-                                          :class => "#{'active' if current_page?(category: c.id)}")
-                                 )
+        child_tree << content_tag('li', category_link(c))
       end
       child_tree << '</ul>'
+    end
+
+    def category_link(category)
+      link_to category.name,
+              {:controller => 'products', :action => 'index', :category => category.id},
+              :class => "#{'active' if current_page?(:category => category.id)}"
     end
 end
